bfb8bb066377ede723b29d7b2cf3067dffd68ae6
Pete Dring <pddring@googlemail.com>
2020-04-20 10:47:41 +0100

    Add project files.

---
+++ Palindromes/Palindrome.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Palindromes
+{
+    partial class Palindrome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(419, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter some text to see if it is a palindrome:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(57, 37);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(186, 31);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.Text = "racecar";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(249, 37);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(125, 32);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(12, 90);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(498, 23);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "Click Check to see if your input is a palindrome";
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Palindrome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 122);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label2);
+            this.Name = "Palindrome";
+            this.Text = "Palindrome Solver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Palindromes/Palindrome.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Palindromes
+{
+    public partial class Palindrome : Form
+    {
+        public Palindrome()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            // read text from text box
+            string input = txtInput.Text;
+            
+            // assume the text is a palindrome until proven otherwise
+            bool palindrome = true;
+            
+            // iterate through each letter up to half way through the text
+            for(int i = 0; i < input.Length / 2; i++)
+            {
+                // check pairs of letters
+                if(input[i] != input[input.Length - 1 - i])
+                {
+                    // stop as soon as we find a pair of letters that doesn't match
+                    palindrome = false;
+                    break;
+                }
+            }
+            
+            // display output
+            if(palindrome)
+            {
+                lblOutput.Text = input + " is a palindrome";
+                lblOutput.ForeColor = Color.Green;
+            } else
+            {
+                lblOutput.Text = input + " is not a palindrome";
+                lblOutput.ForeColor = Color.Red;
+            }
+        }
+    }
+}
